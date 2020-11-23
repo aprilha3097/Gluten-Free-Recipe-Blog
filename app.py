412cd32d02b5e078e2a26a741d1ad0e42bc1f444
aprilha3097 <aprilha3097@gmail.com>
2020-11-23 21:33:40 +0000

    Created a basic navbar inside Base template

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_RURI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
