9755060fd545715710825935b04703db057d2254
aprilha3097 <aprilha3097@gmail.com>
2020-12-02 11:50:55 +0000

    Add route to view recipes on main page

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -9,10 +14,14 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/get_recipes")
+def get_home():
+    recipes = list(mongo.db.recipes.find())
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
